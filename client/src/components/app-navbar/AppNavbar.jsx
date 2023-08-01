b61f7fcf442346823ffe227b695db55517c99df3
ketantb <kbhatkhore777@gmail.com>
2023-08-01 11:38:56 +0530

    added crusor: pointer  at home icon and logo at navbar and added useNavigate at then to redirected the usen on home page

--- client/src/components/app-navbar/AppNavbar.jsx
+++ client/src/components/app-navbar/AppNavbar.jsx
@@ -8,13 +8,13 @@ const AppNavbar = () => {
   const navigate = useNavigate()
   return (
     <div className='appnavbar-wrapper'>
-      <h5>
+      <h5 onClick={() => navigate('/home-page')}>
         HAYAT MART
       </h5>
       <h6>Ai Face Recognition gallery</h6>
       <section className='appbar-right-side-icons'>
         <p>
-          <span style={{ backgroundColor: '#1f282f' }} className='appbar-home-icon'>
+          <span style={{ backgroundColor: '#1f282f' }} className='appbar-home-icon' onClick={() => navigate('/home-page')}>
             <FaHome />
           </span>
         </p>
